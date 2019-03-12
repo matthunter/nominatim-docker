2fdecf4b9f6b36cb3d464a6f79fa87f7092bab47
Matt Hunter <mhunter@myvest.com>
2019-03-11 23:33:37 -0700

    Added Tiger data support

--- 3.1/init.sh
+++ 3.1/init.sh
@@ -1,6 +1,7 @@
 OSMFILE=$1
 PGDIR=$2
 THREADS=$3
+TIGERDIR=$PGDIR/tiger
 
 mkdir -p /data/$PGDIR && \
 
@@ -15,6 +16,9 @@ sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src && \
 sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
-sudo -u postgres psql postgres -tAc "CREATE INDEX nodes_index ON public.planet_osm_ways USING gin (nodes);"
+sudo -u nominatim ./src/build/utils/imports.php --parse-tiger $TIGERDIR && \
+sudo -u nominatim ./src/build/utils/setup.php --import-tiger-data && \
+sudo -u nominatim ./src/build/utils/setup.php --create-functions --enable-diff-updates --create-partition-functions && \ 
+sudo -u postgres psql postgres -tAc "CREATE INDEX nodes_index ON public.planet_osm_ways USING gin (nodes);" 
 sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR stop && \
 sudo chown -R postgres:postgres /data/$PGDIR
