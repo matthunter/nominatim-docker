3743a09fb064b34de336c0d7fdfd3634396d959f
Matt Hunter <mhunter@myvest.com>
2019-03-13 14:13:04 -0700

    Fixes

--- 3.1/init.sh
+++ 3.1/init.sh
@@ -1,7 +1,6 @@
 OSMFILE=$1
 PGDIR=$2
 THREADS=$3
-TIGERDIR=$PGDIR/tiger
 
 mkdir -p /data/$PGDIR && \
 
@@ -16,9 +15,14 @@ sudo -u postgres psql postgres -c "DROP DATABASE IF EXISTS nominatim" && \
 useradd -m -p password1234 nominatim && \
 chown -R nominatim:nominatim ./src && \
 sudo -u nominatim ./src/build/utils/setup.php --osm-file $OSMFILE --all --threads $THREADS && \
-sudo -u nominatim ./src/build/utils/imports.php --parse-tiger $TIGERDIR && \
+echo "STARTING PARSING TIGER DATA" && \
+sudo -u nominatim ./src/build/utils/imports.php --parse-tiger /data/tiger && \
+echo "PARSE COMPLETE" && \
+echo "STARTING IMPORT OF TIGER DATA" && \
 sudo -u nominatim ./src/build/utils/setup.php --import-tiger-data && \
+echo "IMPORTED TIGER DATA" && \
 sudo -u nominatim ./src/build/utils/setup.php --create-functions --enable-diff-updates --create-partition-functions && \ 
+echo "FINISHED FUNCTION UPDATES" && \
 sudo -u postgres psql postgres -tAc "CREATE INDEX nodes_index ON public.planet_osm_ways USING gin (nodes);" 
 sudo -u postgres /usr/lib/postgresql/9.5/bin/pg_ctl -D /data/$PGDIR stop && \
 sudo chown -R postgres:postgres /data/$PGDIR
